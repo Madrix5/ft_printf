8f2c746f8c792bb3bfe03d39df292870a26c2d13
Adrian Jimenez Calderon <adrijime@car8s4.42barcelona.com>
2024-02-06 20:43:03 +0100

    Implementada primera funcion ft_printf, escritos prototipos del checker y del lector de porcentage

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,31 @@
 /*   By: adrijime <adrijime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 20:00:20 by adrijime          #+#    #+#             */
-/*   Updated: 2024/02/06 20:12:14 by adrijime         ###   ########.fr       */
+/*   Updated: 2024/02/06 20:41:26 by adrijime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libftprintf.h"
+#include "ft_printf.h"
 
-int	ft_printf(char const *, ...)
+static void	ft_percent(char	c)
+{
+	return ;
+}
+
+int	ft_checker(va_list ap, char const *str, int res)
 {
 	return (0);
+}
+
+int	ft_printf(char const *str, ...)
+{
+	int		res;
+	va_list	ap;
+
+	res = 0;
+	va_start(ap, str);
+	if (ft_checker(ap, str, res) == -1)
+		return (-1);
+	va_end(ap);
+	return (res);
 }

ac0532d21d5de93ff4b168660d2d53290aeb3bb7
Adrian Jimenez Calderon <adrijime@car12s3.42barcelona.com>
2024-02-13 19:24:50 +0100

    corregido ft_putnbr_base

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: adrijime <adrijime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 20:04:21 by adrijime          #+#    #+#             */
-/*   Updated: 2024/02/13 19:05:49 by adrijime         ###   ########.fr       */
+/*   Updated: 2024/02/13 19:21:41 by adrijime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@
 int	ft_printf(char const *str, ...);
 int	ft_putchar_fd(char c, int fd, int res);
 int	ft_putstr_fd(char *s, int fd, int res);
-int	ft_putnbr_base();
+int	ft_putnbr_base(va_list ap, int base, int res, int sign, int mayus);
 
 #endif

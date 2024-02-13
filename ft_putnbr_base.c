b6c06627f9728029799aeacb97c55d6d346e2656
Adrian Jimenez Calderon <adrijime@car12s3.42barcelona.com>
2024-02-13 19:24:28 +0100

    puestos todos los argumentos

--- ft_putnbr_base.c
+++ ft_putnbr_base.c
@@ -6,13 +6,13 @@
 /*   By: adrijime <adrijime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 19:56:44 by adrijime          #+#    #+#             */
-/*   Updated: 2024/02/13 18:02:20 by adrijime         ###   ########.fr       */
+/*   Updated: 2024/02/13 19:21:51 by adrijime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putnbr_base()
+int	ft_putnbr_base(va_list ap, int base, int res, int sign, int mayus)
 {
 	return (0);
 }

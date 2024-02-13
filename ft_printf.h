d6822bbfb9b16d18b0519837ebc9e8badbf75815
Adrian Jimenez Calderon <adrijime@car12s3.42barcelona.com>
2024-02-13 19:06:48 +0100

    corregido recepcion de res

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: adrijime <adrijime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 20:04:21 by adrijime          #+#    #+#             */
-/*   Updated: 2024/02/13 17:52:28 by adrijime         ###   ########.fr       */
+/*   Updated: 2024/02/13 19:05:49 by adrijime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@
 
 //---------- Funciones ---------//
 int	ft_printf(char const *str, ...);
-int	ft_putchar_fd(char c, int fd);
-int	ft_putstr_fd(char *s, int fd);
-
+int	ft_putchar_fd(char c, int fd, int res);
+int	ft_putstr_fd(char *s, int fd, int res);
+int	ft_putnbr_base();
 
 #endif

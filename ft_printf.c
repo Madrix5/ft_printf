7bd7618f21e4e245d367f94e144674af2f2c85d8
Adrian Jimenez Calderon <adrijime@car12s3.42barcelona.com>
2024-02-13 19:07:22 +0100

    arreglado error de res

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: adrijime <adrijime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 20:00:20 by adrijime          #+#    #+#             */
-/*   Updated: 2024/02/13 16:50:09 by adrijime         ###   ########.fr       */
+/*   Updated: 2024/02/13 19:05:59 by adrijime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 static int	ft_percent(va_list ap, char	c, int res)
 {
-	if (c == 'c' && (ft_putchar_fd(ap, 1)) == -1)
+	if (c == 'c' && (ft_putchar_fd(ap, 1, res)) == -1)
 		return (-1);
-	if (c == 's' && (ft_putstr_fd(ap, 1)) == -1)
+	if (c == 's' && (ft_putstr_fd(ap, 1, res)) == -1)
+		return (-1);
+	if (c == 'p' && (ft_putnbr_base(ap, 16, res)) == -1)
 		return (-1);
 }
 
@@ -34,7 +36,7 @@ static int	ft_checker(va_list ap, char const *str, int res)
 	{
 		while (str[i] && str[i] != '%')
 		{
-			if ((ft_putchar_fd(str[i], 1)) == -1);
+			if ((ft_putchar_fd(str[i], 1, res)) == -1);
 				return (-1);
 			i++;
 			res++;
@@ -68,8 +70,22 @@ int	ft_printf(char const *str, ...)
 int	main(void)
 {
 	char	c;
+	// int		i;
+	// char	*ch;
+	char 	*str;
 
 	c = 'h';
-	ft_printf("Hola, %c\n", c);
+	// i = 54;
+	// ch = &c;
+	str = "Soy Eduardo Manos Tijeras. Te corto el pelo?";
+	ft_printf("Char: %c\n", c);
+	ft_printf("String: %s\n", str);
+	// ft_printf("Pointer: %p\n", ch);
+	// ft_printf("Decimal: %d\n", i);
+	// ft_printf("Entero base 10: %i", i);
+	// ft_printf("Decimal base 10 sin signo: %u", i);
+	// ft_printf("Hexadecimal minus: %u", i);
+	// ft_printf("Hexadecimal mayus: %u", i);
+	ft_printf("%%");
 	return (0);
 }

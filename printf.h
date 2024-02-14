11953827827b2ad8b11c2f5c08ce8d3cf4bd87f2
Adrian Jimenez Calderon <adrijime@car13s5.42barcelona.com>
2024-02-14 18:31:44 +0100

    anadido putnbr

--- printf.h
+++ printf.h
@@ -6,7 +6,7 @@
 /*   By: adrijime <adrijime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 20:04:21 by adrijime          #+#    #+#             */
-/*   Updated: 2024/02/14 17:47:47 by adrijime         ###   ########.fr       */
+/*   Updated: 2024/02/14 18:26:37 by adrijime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@
 # include <unistd.h>
 # include <stdarg.h>
 # include <stdio.h>
+# include <limits.h>
 
 //---------- Funciones ---------//
 int	ft_printf(char const *str, ...);
 int	ft_putchar_fd(char c, int res);
 int	ft_putstr_fd(char *s, int res);
+int	ft_putnbr(int n, int res, int sign);
 int	ft_putnbr_base(va_list ap, int base, int res, int sign, int mayus);
 
 #endif

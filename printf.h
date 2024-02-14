179c72a91a2de3dffab4275e0a4b55e5dc6d312e
Adrian Jimenez Calderon <adrijime@car13s5.42barcelona.com>
2024-02-14 18:21:08 +0100

    anadida unistd y stdio

--- printf.h
+++ printf.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   printf.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adrijime <adrijime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 20:04:21 by adrijime          #+#    #+#             */
-/*   Updated: 2024/02/13 19:21:41 by adrijime         ###   ########.fr       */
+/*   Updated: 2024/02/14 17:47:47 by adrijime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 # define LIBFTPRINTF_H
 
 //---------- Librerias ---------//
+# include <unistd.h>
 # include <stdarg.h>
+# include <stdio.h>
 
 //---------- Funciones ---------//
 int	ft_printf(char const *str, ...);
-int	ft_putchar_fd(char c, int fd, int res);
-int	ft_putstr_fd(char *s, int fd, int res);
+int	ft_putchar_fd(char c, int res);
+int	ft_putstr_fd(char *s, int res);
 int	ft_putnbr_base(va_list ap, int base, int res, int sign, int mayus);
 
 #endif

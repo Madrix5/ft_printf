ed3708849f6605636d6769755fd7123c9e317699
Adrian Jimenez Calderon <adrijime@car13s5.42barcelona.com>
2024-02-14 17:43:07 +0100

    quitado fd

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,31 +6,31 @@
 /*   By: adrijime <adrijime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 17:50:12 by adrijime          #+#    #+#             */
-/*   Updated: 2024/02/13 19:52:21 by adrijime         ###   ########.fr       */
+/*   Updated: 2024/02/14 17:36:47 by adrijime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-int	ft_putchar_fd(char c, int fd, int res)
+int	ft_putchar_fd(char c, int res)
 {
-	if (!fd || !c)
+	if (!c)
 		return (-1);
-	write(fd, &c, 1);
+	write(1, &c, 1);
 	res++;
 	return (res);
 }
 
-int	ft_putstr_fd(char *s, int fd, int res)
+int	ft_putstr_fd(char *s, int res)
 {
 	int	i;
 
 	i = 0;
-	if (!s || !fd)
+	if (!s)
 		return (-1);
 	while (s[i] != '\0')
 	{
-		write(fd, &s[i], 1);
+		write(1, &s[i], 1);
 		i++;
 		res++;
 	}
